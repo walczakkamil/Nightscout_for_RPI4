3ac6ddaa325859382255602edd67400a6b6ac315
Kamil Walczak <waperr@interia.pl>
2021-03-03 12:16:04 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker build -t waperr/nightscout-rpi .
+source .env
+
+echo "### Pull or Build Docker image"
+if ! docker pull waperr/nightscout-rpi:${NS_RELEASE}; then
+    docker build -t waperr/nightscout-rpi:${NS_RELEASE} .
+fi
